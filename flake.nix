5d84cc6bf6bcab711e3be55f1ec88a423a73caf9
Gerard Du Pre <hi@gerarddupre.dev>
2026-08-15 21:48:21 +0200

    feat: :sparkles: nix flake (#63)
    feat: nix flake

---
+++ flake.nix
@@ -0,0 +1,85 @@
+{
+  description = "A modern highly-customizable Subsonic TUI client built in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./ratune/Cargo.toml);
+
+        ratune = pkgs.rustPlatform.buildRustPackage {
+          pname = "ratune";
+          version = cargoToml.package.version;
+
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+            pkgs.alsa-lib
+            pkgs.dbus
+            pkgs.openssl
+          ];
+
+          meta = with pkgs.lib; {
+            description = "A modern highly-customizable Subsonic TUI client built in Rust";
+            homepage = "https://github.com/acmagn/ratune";
+            license = licenses.mit;
+            mainProgram = "ratune";
+          };
+        };
+      in
+      {
+        packages.default = ratune;
+        packages.ratune = ratune;
+
+        apps.default = {
+          type = "app";
+          program = "${ratune}/bin/ratune";
+        };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ ratune ];
+          nativeBuildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rustfmt
+          ];
+        };
+      }
+    )) // {
+      overlays.default = final: prev: {
+        ratune = self.packages.${prev.system}.default;
+      };
+
+      homeManagerModules.default = { config, lib, pkgs, ... }:
+        let
+          cfg = config.programs.ratune;
+        in
+        {
+          options.programs.ratune = {
+            enable = lib.mkEnableOption "Ratune Subsonic TUI music player";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${pkgs.system}.default;
+              description = "The ratune package to install.";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            home.packages = [ cfg.package ];
+          };
+        };
+    };
+}
